6314c25bc39221cd55fa5db2a17a93e2e166b910
LukeNoles <89222532+LukeNoles@users.noreply.github.com>
2022-09-06 23:23:31 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,59 +1,89 @@
 ﻿using System;
-using static System.Net.Mime.MediaTypeNames;
 
-namespace palindromechecker
+namespace PracticeExcercise0
 {
-    class Program
+    public class Program
     {
         public static bool IsPalindrome(string s)
         {
-            int s_length = s.Length;
-            for (int checker = 0; checker < s_length / 2; checker++)
-            {
-                if (s[checker] != s[s_length - checker - 1])
-                    return false;
-            }
-            return true;
+            string cleanString = s.ToLower().Replace(" ", "");
 
+            string reversed = Reverse(cleanString);
+
+            return (reversed == cleanString);
         }
-    }
-}
 
-namespace reversal
-{
-    class Program
-    {
+        private static string Reverse(string s)
+        {
+            string result = "";
+
+            foreach( char c in s)
+            {
+                result = result.Insert(0, c.ToString() );
+            }
+
+            return result;
+        }
+
+
         public static int ReverseInt(int i)
         {
-            int reverseInt = 0;
-            while (i > 0)
-            {
-                reverseInt = (reverseInt * 10) + (i % 10);
-                i = i / 10;
-            }
-            return reverseInt;
-        }
-    }
-}
+            int x = i;
+            int reverse = 0;
 
-namespace unique
-{
-    class Program
-    {
+            while( x != 0 )
+            {
+                int remainder = x % 10;
+                reverse = reverse * 10 + remainder;
+                x = x / 10;
+            }
+
+            return reverse;
+        }
+
+
         public static bool IsUnique(string s)
         {
+            string cleanString = s.ToLower().Replace(" ", "");
 
+            for( int i=0; i < cleanString.Length; i++)
+            {
+
+                for(int j=i+1; j < cleanString.Length; j++)
+                {
+                    if (cleanString[i] == cleanString[j])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
         }
-    }
-}
 
-namespace neil
-{
-    class Program
-    {
         public static bool IsNeilNumber(int i)
         {
+            int nDigits = i.ToString().Length;
+            int count = 0;
+            int bottom = i.ToString()[count];
+            double equation1 = Math.Pow(bottom, nDigits);
+            double equation2 = 0;
 
+            while (count < nDigits) ;
+            {
+                count += 1;
+                equation2 = equation1 + equation2;
+            }
+            if (equation2 == i) ;
+            {
+                return true;
+            }
+            else;
+            {
+                return false;
+            }
+            
         }
+
     }
 }
